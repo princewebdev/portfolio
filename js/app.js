fb8f2fbc66e14c5dd33345e45445487567b752ac
princewebdev <coderboyprince@gmail.com>
2024-08-08 09:03:02 +0600

    v1.0

---
+++ js/app.js
@@ -0,0 +1,108 @@
+/*
+ * typingEffect()
+ * It types an array of texts in a random order. I like random stuff🙃
+ */
+function typingEffect() {
+    const contactTexts = shuffleArray(['Ask me anything!😄','Wordpress Developer😊', 'Web Developer🧑‍💻', 'Shopify Expert🤗']);
+    const typedtext = document.getElementsByClassName("typedtext")[0];
+    let removing = false;
+    let idx = char = 0;
+
+    setInterval(() => { // We define the interval of the typing speed
+
+        // If we do not reach the limit, we insert characters in the html
+        if (char < contactTexts[idx].length) typedtext.innerHTML += contactTexts[idx][char];
+
+        // 15*150ms = time before starting to remove characters
+        if (char == contactTexts[idx].length + 15) removing = true;
+
+        // Removing characters, the last one always
+        if (removing) typedtext.innerHTML = typedtext.innerHTML.substring(0, typedtext.innerHTML.length - 1);
+
+        char++; // Next character
+
+        // When there is nothing else to remove
+        if (typedtext.innerHTML.length === 0) {
+
+            // If we get to the end of the texts we start over
+            if (idx === contactTexts.length - 1) idx = 0
+            else idx++;
+
+            char = 0; // Start the next text by the first character
+            removing = false; // No more removing characters
+        }
+
+    }, 150); // Typing speed, 150 ms
+
+}
+typingEffect();
+function shuffleArray(array) {
+    let currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
+// slick slider
+$('.slickSlider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+});
+// form validation
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
